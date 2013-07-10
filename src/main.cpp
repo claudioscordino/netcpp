7aca2a6d1cd5081d9ace9d729cd945d340edae4d
Claudio Scordino <claudio@evidence.eu.com>
2013-07-10 15:05:13 +0200

    First passed test

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,26 @@ void do_server()
 
 
 	// sleep
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-
+// 	std::this_thread::sleep_for(std::chrono::seconds(2));
 
 	while (true){
 		DEBUG(DEBUG, "SERVER: Creating buffer...");
 		std::array<char, 5> b;
 		DEBUG(DEBUG, "SERVER: Reading...");
 		srv2.read(net::buffer(b), 3);
+		DEBUG(DEBUG, "SERVER: Received: " << b[0] << " "
+			<< b[1] << " " 
+			<< b[2] << " "
+			<< b[3] << " "
+			<< b[4]);
 	}
 }
 
 
 void do_client()
 {
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
 	DEBUG(DEBUG, "CLIENT: Creating address...");
 	net::ip4::tcp::address addr (std::string("127.0.0.1"), 1234);
 	DEBUG(DEBUG, "CLIENT: Creating client...");
@@ -43,13 +49,10 @@ void do_client()
 	DEBUG(DEBUG, "CLIENT: Opening client...");
 	clt.open(&addr);
 
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-
-
 
 	while (true){
 		DEBUG(DEBUG, "CLIENT: Creating buffer...");
-		std::array<char, 5> b;
+		std::array<char, 5> b {'c', 'i', 'a', 'o'};
 		DEBUG(DEBUG, "CLIENT: Writing...");
 		clt.write(net::buffer(b), 3);
 	}
@@ -57,7 +60,7 @@ void do_client()
 
 int main()
 {
-	DEBUG_CONF("/tmp/netwing.txt", DEBUG, DEBUG);
+	DEBUG_CONF("/tmp/libnetpp/log.txt", DEBUG, DEBUG);
 	DEBUG(DEBUG, "Launching 1st thread...");
 	std::thread t1(do_server);
 	DEBUG(DEBUG, "Launching 2nd thread...");
