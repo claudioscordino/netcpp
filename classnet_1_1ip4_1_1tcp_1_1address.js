10296ec2a549521ae055890572c1e929705da986
Claudio Scordino <claudio@evidence.eu.com>
2013-07-16 17:25:54 +0200

    Documentation updated

--- classnet_1_1ip4_1_1tcp_1_1address.js
+++ classnet_1_1ip4_1_1tcp_1_1address.js
@@ -1,7 +1,6 @@
 var classnet_1_1ip4_1_1tcp_1_1address =
 [
     [ "address", "classnet_1_1ip4_1_1tcp_1_1address.html#af6ae4e1f8456029f5fbfe3571c31ace5", null ],
-    [ "address", "classnet_1_1ip4_1_1tcp_1_1address.html#af7b3db82ea76900bf0727eeeab10a489", null ],
     [ "getAddress", "classnet_1_1ip4_1_1tcp_1_1address.html#a211cb9c49487972650a67dd31175846c", null ],
     [ "getPort", "classnet_1_1ip4_1_1tcp_1_1address.html#a5e4fc3488ccbe0511b7e7a1b3ca873cf", null ],
     [ "address_", "classnet_1_1ip4_1_1tcp_1_1address.html#a1e2ddd57de288f7ee0529bbd1fb50a8e", null ],
--- search/all_61.js
+++ search/all_61.js
@@ -11,7 +11,7 @@ var searchData=
   ['address',['address',['../classnet_1_1local_1_1stream_1_1address.html',1,'net::local::stream']]],
   ['address',['Address',['../classnet_1_1Address.html',1,'net']]],
   ['address',['address',['../classnet_1_1ip4_1_1tcp_1_1address.html',1,'net::ip4::tcp']]],
-  ['address',['address',['../classnet_1_1local_1_1stream_1_1address.html#a578eb323307f0caf9aa5fd83099c80e7',1,'net::local::stream::address::address()'],['../classnet_1_1ip4_1_1tcp_1_1address.html#af6ae4e1f8456029f5fbfe3571c31ace5',1,'net::ip4::tcp::address::address(const std::string &amp;addr, int port)'],['../classnet_1_1ip4_1_1tcp_1_1address.html#af7b3db82ea76900bf0727eeeab10a489',1,'net::ip4::tcp::address::address()'],['../namespacenet_1_1local_1_1dgram.html#a39484c3ba9ee81b13bd5fe07d22ee794',1,'net::local::dgram::address()'],['../namespacenet_1_1ip4_1_1udp.html#a34c6d4d06a562ccbc06ba346c1bf27e6',1,'net::ip4::udp::address()']]],
+  ['address',['address',['../classnet_1_1local_1_1stream_1_1address.html#a578eb323307f0caf9aa5fd83099c80e7',1,'net::local::stream::address::address()'],['../classnet_1_1ip4_1_1tcp_1_1address.html#af6ae4e1f8456029f5fbfe3571c31ace5',1,'net::ip4::tcp::address::address()'],['../namespacenet_1_1local_1_1dgram.html#a39484c3ba9ee81b13bd5fe07d22ee794',1,'net::local::dgram::address()'],['../namespacenet_1_1ip4_1_1udp.html#a34c6d4d06a562ccbc06ba346c1bf27e6',1,'net::ip4::udp::address()']]],
   ['address_2ehpp',['address.hpp',['../address_8hpp.html',1,'']]],
   ['address_5f',['address_',['../classnet_1_1local_1_1stream_1_1address.html#a1e2ddd57de288f7ee0529bbd1fb50a8e',1,'net::local::stream::address::address_()'],['../classnet_1_1ip4_1_1tcp_1_1address.html#a1e2ddd57de288f7ee0529bbd1fb50a8e',1,'net::ip4::tcp::address::address_()']]]
 ];
